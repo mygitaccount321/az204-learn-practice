f33f07394f57bc2b5e215bc87b630abebe010ac9
Vishwanath <vishwa.naik189@gmail.com>
2024-08-25 14:31:38 +0530

    Timer Trigger with retry added

--- Functions/TimerTrigger/src/main/java/org/example/functions/TimerTriggerJava.java
+++ Functions/TimerTrigger/src/main/java/org/example/functions/TimerTriggerJava.java
@@ -18,4 +18,24 @@ public class TimerTriggerJava {
     ) {
         context.getLogger().info("Java Timer trigger function executed at: " + LocalDateTime.now());
     }
+
+    public static int count = 1;
+
+    //Retry policies will work only for Cosmos DB, Event Hub, Kafka, Timer
+    //https://learn.microsoft.com/en-us/azure/azure-functions/functions-bindings-error-pages?tabs=fixed-delay%2Cisolated-process%2Cnode-v4%2Cpython-v2&pivots=programming-language-java
+    @FunctionName("TimerTriggerRetry")
+    @FixedDelayRetry(maxRetryCount = 1, delayInterval = "00:00:01")
+    public void HttpExampleRetry1(
+            @TimerTrigger(name = "timerInfo", schedule = "0 0/5 * * * *") String timerInfo,
+            final ExecutionContext context) throws Exception {
+        context.getLogger().info("====1==============================Java HTTP1 trigger processed a request.");
+
+        if (count < 1) {
+            count++;
+            context.getLogger().info("==================================Java HTTP trigger processed a request-Error");
+            throw new Exception("error");
+        }
+        context.getLogger().info("=====================Timer is triggered: " + timerInfo);
+        count = 0;
+    }
 }
