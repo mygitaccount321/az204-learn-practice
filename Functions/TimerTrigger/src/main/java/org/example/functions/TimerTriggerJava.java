b73aae156e13183e2fc536aaf83c53a57fef08b4
Vishwanath <vishwa.naik189@gmail.com>
2024-08-25 14:20:01 +0530

    Timer Trigger added

---
+++ Functions/TimerTrigger/src/main/java/org/example/functions/TimerTriggerJava.java
@@ -0,0 +1,21 @@
+package org.example.functions;
+
+import java.time.*;
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+/**
+ * Azure Functions with Timer trigger.
+ */
+public class TimerTriggerJava {
+    /**
+     * This function will be invoked periodically according to the specified schedule.
+     */
+    @FunctionName("TimerTriggerJava")
+    public void run(
+        @TimerTrigger(name = "timerInfo", schedule = "0 * * * * *") String timerInfo,
+        final ExecutionContext context
+    ) {
+        context.getLogger().info("Java Timer trigger function executed at: " + LocalDateTime.now());
+    }
+}
