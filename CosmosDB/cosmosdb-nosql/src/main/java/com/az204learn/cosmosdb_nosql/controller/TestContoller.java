650273113862e0c1ba8f16d2dbfb61b45d55c21b
Vishwanath <vishwa.naik189@gmail.com>
2024-09-04 20:04:02 +0530

    Simple Cosmos DB no Sql added

--- CosmosDB/cosmosdb-nosql/src/main/java/com/az204learn/cosmosdb_nosql/controller/TestContoller.java
+++ CosmosDB/cosmosdb-nosql/src/main/java/com/az204learn/cosmosdb_nosql/controller/TestContoller.java
@@ -2,10 +2,14 @@ package com.az204learn.cosmosdb_nosql.controller;
 
 import com.az204learn.cosmosdb_nosql.models.Student;
 import com.az204learn.cosmosdb_nosql.repository.StudentRepository;
+import com.sun.jna.platform.win32.Guid;
+import jakarta.websocket.server.PathParam;
 import jdk.jfr.Frequency;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class TestContoller {
@@ -18,15 +22,26 @@ public class TestContoller {
         return "Hi Cosmos DB";
     }
 
-    @GetMapping("/student")
-    public Student createst() {
-        Student st = new Student(1, "2", "Vishwa" );
-        studentRepository.save(st);
-        return st;
+    @PostMapping("/student")
+    public Student createst(@RequestBody Student student) {
+        student.setId(UUID.randomUUID().toString());
+        studentRepository.save(student);
+        return student;
     }
 
-    @GetMapping("/student1")
-    public Student createst1() {
-        return studentRepository.findById("1").get();
+    @PutMapping("/student")
+    public Student createstionUpdate(@RequestBody Student student) {
+        studentRepository.save(student);
+        return student;
+    }
+
+    @GetMapping("/student/{id}")
+    public Student createst1(@PathVariable String id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @GetMapping("/students")
+    public List<Student> getAllStudentInGrade(@PathParam("grade") String grade) {
+        return studentRepository.getAllStudentInGrade(grade);
     }
 }
--- CosmosDB/cosmosdb-nosql/src/main/java/com/az204learn/cosmosdb_nosql/repository/StudentRepository.java
+++ CosmosDB/cosmosdb-nosql/src/main/java/com/az204learn/cosmosdb_nosql/repository/StudentRepository.java
@@ -2,8 +2,15 @@ package com.az204learn.cosmosdb_nosql.repository;
 
 import com.az204learn.cosmosdb_nosql.models.Student;
 import com.azure.spring.data.cosmos.repository.CosmosRepository;
+import com.azure.spring.data.cosmos.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends CosmosRepository<Student, String> {
+
+    @Query("SELECT  * From student s where s.grade= @grade")
+    List<Student> getAllStudentInGrade(@Param("grade") String grade);
 }
