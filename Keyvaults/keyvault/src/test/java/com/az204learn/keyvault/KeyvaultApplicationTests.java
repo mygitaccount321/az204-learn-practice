6aaaa8fcd5f1cb47a1afa3187e6b0654769ea731
Vishwanath <vishwa.naik189@gmail.com>
2024-09-17 22:23:48 +0530

    Added keyvaults

---
+++ Keyvaults/keyvault/src/test/java/com/az204learn/keyvault/KeyvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.az204learn.keyvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
